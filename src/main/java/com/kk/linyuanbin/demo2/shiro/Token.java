5c07fa45495b7ca53a60fa1d9d3a260634d8cbbc
marvellin <1021478667@qq.com>
2020-08-31 18:30:15 +0800

    0831 完成Token

--- src/main/java/com/kk/linyuanbin/demo2/shiro/Token.java
+++ src/main/java/com/kk/linyuanbin/demo2/shiro/Token.java
@@ -1,19 +1,22 @@
 package com.kk.linyuanbin.demo2.shiro;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.apache.shiro.authc.AuthenticationToken;
 
+//自定义token 实现Authenticationtoken 认证需要用到
+@Data
+@NoArgsConstructor
 public class Token implements AuthenticationToken {
-    public Token(String token) {
-
-    }
+    private String token;
 
     @Override
     public Object getPrincipal() {
-        return null;
+        return this.getToken();
     }
 
     @Override
     public Object getCredentials() {
-        return null;
+        return this.getToken();
     }
 }
